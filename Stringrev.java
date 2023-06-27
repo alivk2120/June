d609cca5a4b046327c4922d40c6fc5f8648b9b5a
ali <alivk22120@gmail.com>
2023-06-27 09:54:23 +0530

    first commit

--- Stringrev.java
+++ Stringrev.java
@@ -1,8 +1,7 @@
+//WAP to find reverse of a string.
 class Stringrev {
   public static void main(String args[]) {
- 
     String str = "Hello";
- 
     for (int i = str.length()-1; i>=0; i--) {
       System.out.print(str.charAt(i));
     }
